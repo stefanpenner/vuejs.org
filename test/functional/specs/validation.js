d1ca6585a05fab19ac391ddcaa5cadb0afce3c2d
Evan You <yyou@google.com>
2014-01-15 19:06:08 -0500

    Site updated: 2014-01-15 19:06:08

---
+++ test/functional/specs/validation.js
@@ -0,0 +1,26 @@
+casper.test.begin('Validation', 4, function (test) {
+    
+    casper
+    .start('./fixtures/validation.html')
+    .then(function () {
+        test.assertElementCount('.valid', 0)
+        this.sendKeys('input', '@hello.com')
+    })
+    .then(function () {
+        test.assertElementCount('.valid', 1)
+    })
+    .thenEvaluate(function () {
+        document.querySelector('input').setSelectionRange(4,4)
+    })
+    .then(function () {
+        this.sendKeys('input', 'hoho')
+    })
+    .then(function () {
+        test.assertElementCount('.valid', 1)
+        test.assertField('email', 'hihihoho@hello.com')
+    })
+    .run(function () {
+        test.done()
+    })
+
+})
